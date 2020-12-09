88e498ae49dd90dd63cce33fdd6f0794436dba4d
Danielle C <72367042+dan1ellec@users.noreply.github.com>
2020-12-09 22:00:57 +1100

    working out removed and code commented

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -30,93 +30,25 @@ with open(csvpath) as csvfile:
         candidate_votes.append(row[2])
 
 
-
-
-    #what we are looking for
-
-    #The total number of votes cast
-    #could be caluculated by counting any new list: 3521001
-
+    #the total number of votes cast
     total_votes = len(voter_id)
-    #total_county = len(county)
-    #total_candidate = len(candidate_votes)
+    
 
-    #print(f"{total_votes}")
-    #print(f"{total_county}")
-    #print(f"{total_candidate}")
-
-
-    #A complete list of candidates who received votes
-    #so have to go through candidate votes and note every new person
-
+    #a complete list of candidates who received votes
+    #looping through all votes and adding candidates to new list only if not already added 
     candidates = []
-    #candidates.append(candidate_votes[0])
-    #print(f"{candidates}")
-
-    #for i in range(10):
-    #    for j in range(len(candidates)):
-    #        if (candidate_votes[i] != candidates[j]):
-    #            candidates.append(candidate_votes[i])
-
     for i in candidate_votes:
             if i not in candidates:
                 candidates.append(i)
 
 
-    #candidates_length = len(candidates)
-    #print(f"{candidates_length}")
-    #print(f"{candidates}")
-
-    #so candidate_votes contains all the votes for the candidates 
-    # how many times their name is listed will be how many votes
-    #candidates just has the four candidates
-
-
-    #The total number of votes each candidate won
-    
-    #candidate_0_count = 0
-    
-    #for j in candidates:
-    #for k in candidate_votes:
-    #    if candidates[0] == k:
-    #        candidate_0_count = candidate_0_count + 1
-
-    #print(f"{candidate_0_count}")
-
-    #candidate_1_count = 0
-    
-    #for j in candidates:
-    #for k in candidate_votes:
-    #    if candidates[1] == k:
-    #        candidate_1_count = candidate_1_count + 1
-
-    #print(f"{candidate_1_count}")
-
-    #candidate_2_count = 0
-    
-    #for j in candidates:
-    #for k in candidate_votes:
-    #    if candidates[2] == k:
-    #        candidate_2_count = candidate_2_count + 1
-
-    #print(f"{candidate_2_count}")
-
-    #candidate_3_count = 0
-    
-    #for j in candidates:
-    #for k in candidate_votes:
-    #    if candidates[3] == k:
-    #        candidate_3_count = candidate_3_count + 1
-
-    #print(f"{candidate_3_count}")
-
-    #print(candidate_0_count + candidate_1_count + candidate_2_count + candidate_3_count)
-
-    #maybe need to try with a list
-
+    #a list of the total number of votes each candidate won
     votes_per_candidate = []
     votes = 0
 
+    #looping through the individual candidates in the candidates list
+    #checking how many times their name appears in candidate_votes
+    #adding the total number of votes to the new list before moving to the next candidate
     for j in candidates:
         for k in candidate_votes:
             if j == k:
@@ -126,32 +58,23 @@ with open(csvpath) as csvfile:
         votes = 0
     next 
 
-    #print(f"{votes_per_candidate}")
-
-
-
+    
     #The percentage of votes each candidate won
-    #Feel like it need to find the number first 
-
-    #percentage_votes = [round((v/total_votes)*100, 5) for v in votes_per_candidate]
-    #print(f"{percentage_votes}")
-
+    #using list comprehension to create a new list 
+    #where the values in votes_per_candidate have been converted to percentages
     percentage_votes = ["{:.3%}".format(v/total_votes) for v in votes_per_candidate]
-    #print(f"{percentage_votes}")
-
+    
 
     #The winner of the election based on popular vote.   
-
     #finding the index of the greatest votes
     max_votes = max(votes_per_candidate)
     max_index = votes_per_candidate.index(max_votes)
     winner = candidates[max_index]
 
 
-    #will need to zip three lists together to print and export
-    # have candidates, votes_per_candidate and percentage_votes
-    # all in order with corresponding index's
-
+    #zip the three lists together to print and export
+    #have candidates, votes_per_candidate and percentage_votes
+    #all in order with corresponding index's
     votes_summary = zip(candidates, votes_per_candidate, percentage_votes)
 
 
